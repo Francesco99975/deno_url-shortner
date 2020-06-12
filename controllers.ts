1dbb94e77b81dd94cedc00a9ca75db36f7d09e7f
Francesco Michele Barranca <francescomich99@gmail.com>
2020-06-12 14:47:59 -0400

    made some adjustments

--- controllers.ts
+++ controllers.ts
@@ -10,23 +10,39 @@ const shortenUrl = async (
   try {
     const data: any = await request.body();
     const url: string = data.value.url;
-    const rnd = Math.floor(Math.random() * 7 + 1);
-    const slug = nanoid(rnd);
-    console.log(url + " - " + slug);
 
     await client.connect();
-    const result = await client.query(
-      "INSERT INTO urls(url, slug) VALUES($1, $2)",
-      url,
-      slug,
-    );
-    console.log(result);
 
-    response.status = 201;
-    response.body = {
-      success: true,
-      newUrl: "fmb.io" + "/" + slug,
-    };
+    const exists = await client.query(
+      "SELECT slug FROM urls WHERE url=$1",
+      url,
+    );
+
+    const host: string = request.headers.get("host");
+
+    if (exists.rows.toString() === "") {
+      const rnd = Math.floor(Math.random() * 5 + 2);
+      const slug = nanoid(rnd);
+      console.log(url + " - " + slug);
+
+      await client.query(
+        "INSERT INTO urls(url, slug) VALUES($1, $2)",
+        url,
+        slug,
+      );
+
+      response.status = 201;
+      response.body = {
+        success: true,
+        newUrl: host + "/" + slug,
+      };
+    } else {
+      response.status = 201;
+      response.body = {
+        success: true,
+        newUrl: host + "/" + exists.rows[0][0],
+      };
+    }
   } catch (error) {
     console.log(error.message);
     response.status = 500;
