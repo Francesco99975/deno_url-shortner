2b7b18e75032febb6afb45745fdc6b10de23dc9a
Francesco Michele Barranca <francescomich99@gmail.com>
2020-06-12 15:27:57 -0400

    test 2

--- config.prod.ts
+++ config.prod.ts
@@ -2,6 +2,7 @@ const dbCreds = {
   user: Deno.env.get("DB_USER"),
   database: Deno.env.get("DB_NAME"),
   hostname: Deno.env.get("DB_HOST"),
+  password: Deno.env.get("DB_PASSWORD"),
   port: 5432,
 };
 
